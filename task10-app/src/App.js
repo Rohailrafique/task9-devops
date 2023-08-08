22ea376aa309370b8b116a1b9865c34f2cfe2cba
Rohail Rafique <rohailrafique@gmail.com>
2023-08-08 15:26:44 -0400

    skhjbkjdbnja

--- task10-app/src/App.js
+++ task10-app/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to My App</h1>
-      <p>This is a simple React app.</p>
+      <p>This is a simple sfeagjnkjsnbkjfdsnbkjfsnbkjsnkjbsdReact app.</p>
     </div>
   );
 }
